612152577c88b8901e3fcf5594f2d81a4c23c4d2
Michael Lex <michael.lex@codecentric.de>
2014-06-27 00:26:11 +0200

    Add some graphs for prediction of all-vs-all games and confusion matrix for group-games.

---
+++ 6-evaluate/README.md
@@ -0,0 +1,30 @@
+Konfusionsmatrix für die Vohersage der WM-Vorrunde 2014
+=======================================================
+
+          Reference
+Prediction AWAY_WIN DRAW HOME_WIN
+  AWAY_WIN        9    5        6
+  DRAW            0    0        0
+  HOME_WIN       11    4       13
+
+Overall Statistics
+                                          
+               Accuracy : 0.4583          
+                 95% CI : (0.3137, 0.6083)
+    No Information Rate : 0.4167          
+    P-Value [Acc > NIR] : 0.32811         
+                                          
+                  Kappa : 0.0904          
+ Mcnemar's Test P-Value : 0.01496         
+
+Statistics by Class:
+
+                     Class: AWAY_WIN Class: DRAW Class: HOME_WIN
+Sensitivity                   0.4500      0.0000          0.6842
+Specificity                   0.6071      1.0000          0.4828
+Pos Pred Value                0.4500         NaN          0.4643
+Neg Pred Value                0.6071      0.8125          0.7000
+Prevalence                    0.4167      0.1875          0.3958
+Detection Rate                0.1875      0.0000          0.2708
+Detection Prevalence          0.4167      0.0000          0.5833
+Balanced Accuracy             0.5286      0.5000          0.5835
--- 6-evaluate/scripts/vorrunde-confusion-matrix.R
+++ 6-evaluate/scripts/vorrunde-confusion-matrix.R
@@ -1,37 +1,34 @@
-source("scripts/prepare-soccer-data.R")
+source("../4-train-model/scripts/prepare-soccer-data.R")
 library(caret)
 
-load(file="output/trained-random-forest-models.RData")
+load(file="../4-train-model/output/trained-random-forest-models.RData")
 
 wm2014 <- subset(data, b_tournament_name=="WM" & b_tournament_year=="2014" & b_tournament_phase=="Endrunde")
 wm2014_group <- subset(wm2014, b_tournament_group != "")
 
-prediction_prob <- predict(
-  test_results$em_and_wm_with_qualification.all.notdiff.randomForest$fitted, 
-  newdata=wm2014_group,
-  type="prob")
 prediction_raw <- predict(
   test_results$em_and_wm_with_qualification.all.notdiff.randomForest$fitted, 
   newdata=wm2014_group,
   type="raw")
 
+data_with_results <- read.csv2("input/wm-2014-vorrunde-with-result.csv", colClasses="character")
+
 result <- cbind(
-  wm2014_group[,c("b_team_home","b_team_away")], 
-  prediction_prob[c("HOME_WIN","AWAY_WIN")],
-  predicted_class=prediction_raw,
-  actual_class=wm2014_group$r_game_outcome_before_penalties
+  wm2014_group[,c("b_date", "b_team_home","b_team_away")], 
+  predicted_class=prediction_raw
 )
+full_result <- merge(result, data_with_results, by=c("b_date","b_team_home","b_team_away"))
 
-print("Vorhersage für die Vorrunde")
-print("===========================")
-print(result);
+towrite <- full_result[c("b_date","b_team_home","b_team_away","predicted_class","r_game_outcome_before_penalties"),]
+colnames(towrite) <-c("b_date","b_team_home","b_team_away","predicted_class","actual_class") 
+write.csv2(full_result, "output/vorrunde-prediction-with-reference.csv")
 
-result$predicted_class <- factor(result$predicted_class, levels=c("AWAY_WIN","DRAW","HOME_WIN"))
-result$actual_class <- factor(result$actual_class, levels=c("AWAY_WIN","DRAW","HOME_WIN"))
-notmissing <- !is.na(result$actual_class)
+full_result$predicted_class <- factor(full_result$predicted_class, levels=c("AWAY_WIN","DRAW","HOME_WIN"))
+full_result$actual_class <- factor(full_result$r_game_outcome_before_penalties, levels=c("AWAY_WIN","DRAW","HOME_WIN"))
+notmissing <- !is.na(full_result$actual_class)
 
 result_confusion <- confusionMatrix(
-  data=result$predicted_class[notmissing], 
-  reference=result$actual_class[notmissing])
+  data=full_result$predicted_class[notmissing], 
+  reference=full_result$actual_class[notmissing])
 
 print(result_confusion)
